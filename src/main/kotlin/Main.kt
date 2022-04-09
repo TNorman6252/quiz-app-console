84333df721e32a26f4b9bd936554f890b7e9a94c
Tristan <tristan6252@gmail.com>
2022-04-09 14:56:38 +0100

    Added the 'addQuestion()' function/feature to the quiz app along with some user validation.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
+import controllers.QuestionAPI
+import models.Question
 import utils.ScannerInput
+import utils.ScannerInput.readNextLine
+import java.lang.Integer.parseInt
+
+private val qAPI = QuestionAPI();
 
 fun main(args: Array<String>) {
-
+    runMenu();
 }
 
 fun mainMenu() : Int {
@@ -9,19 +15,100 @@ fun mainMenu() : Int {
         > ----------------------------------
         > |        QUIZ APPLICATION        |
         > ----------------------------------
-        > | QUIZ OPTIONS:                      |
-        > |   1) Play Quiz Game               |
-        > |   2) Add a Question                |
-        > |   3) Delete a Question           |
-        > |   4) Number of Questions in App           |
-        > |   5) Update a Question
-        > |   6) List Questions - (Sub-menu)
-        > |   7) Save Question(s)
-        > |   8) Load Question(s)
+        > | QUIZ OPTIONS:                  |
+        > |   1) Play Quiz Game            |
+        > |   2) Add a Question            |   
+        > |   3) Delete a Question         |  
+        > |   4) Number of Questions in App |         
+        > |   5) Update a Question          |
+        > |   6) List Questions - (Sub-menu) |
+        > |   7) Save Question(s) (NOT IMPLEMENTED YET) |
+        > |   8) Load Question(s) (NOT IMPLEMENTED YET) |
         > ----------------------------------
         > |   0) Exit                      |
         > ----------------------------------
         > ==>> """.trimMargin(">"))
 }
+
+fun runMenu() {
+    do {
+        val option = mainMenu();
+        when(option) {
+       //     1 -> playGame();
+            2 -> addQuestion();
+        //    3 -> deleteQuestion();
+         //   4 -> numOfQuestions();
+         //   5 -> updateQuestion();
+          //  6 -> listQuestions();
+         //   7 -> saveQuestions();
+        //    8 -> loadQuestions();
+          //  0 -> exitApp();
+            else -> println("Invalid choice entered: $option. Please try again!");
+        }
+    } while(true);
+}
+
+fun addQuestion() {
+    while(true) {
+        println("----- Adding a Question -----\n");
+
+        var questionNumber = 0;
+        while (true) {
+            try {
+                questionNumber = parseInt(readNextLine("Enter the Question Number here: "));
+                break;
+            } catch (nfe: NumberFormatException) {
+                println("Please enter a number!");
+            } catch (e : Exception) {
+                println("Something went wrong!");
+            }
+        }
+
+        var theQuestion = readNextLine("Enter the Question here: ");
+
+        var possibleAnswers: Array<String?> = arrayOfNulls(4);
+
+        for (i in 0 until possibleAnswers.size-1) {
+            println("Possible Answer (" + (i + 1) + " of 3): ");
+            println("------------------------------------------");
+            possibleAnswers[i] = readNextLine("Enter possible answer here: ");
+        }
+
+        var answer = readNextLine("Enter the answer to the question here: ");
+
+        var questionDifficultyLevel = 0;
+        while (true) {
+            try {
+                questionDifficultyLevel = parseInt(readNextLine("Enter question difficulty level here (1-5):"));
+                break;
+            } catch (nfe: NumberFormatException) {
+                println("Please enter a number!");
+            } catch (e: Exception) {
+                println("Something went wrong!");
+            }
+        }
+
+        var isAdded =
+            qAPI.addQuestion(Question(questionNumber, theQuestion, possibleAnswers, answer, questionDifficultyLevel));
+
+        if (isAdded) {
+            println("Successfully Added Question to System!");
+        } else {
+            println("Something went wrong! Please try again!");
+        }
+
+        println("------------------------------------------------------");
+
+        var addAnotherQuestionAnswer = readNextLine("Would you like to add another question? (Y/N):");
+        if(addAnotherQuestionAnswer.uppercase() != "Y") {
+            println("Heading back to the main menu!");
+            println("--------------------------------------")
+            break;
+        }
+    }
+
+}
+
+
 
 
--- src/main/kotlin/models/Question.kt
+++ src/main/kotlin/models/Question.kt
@@ -3,7 +3,7 @@ package models
 data class Question(
     var questionNumber: Int,
     var theQuestion: String,
-    var possibleAnswers : Array<String>,
+    var possibleAnswers: Array<String?>,
     var questionAnswer: String,
     var questionDifficultyLevel: Int
 ){
